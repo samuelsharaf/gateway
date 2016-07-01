e3eddc5d99dbc9c98468afaa3d63851f272d174d
michrome <m+nitrous@j4e.name>
2016-07-01 21:56:57 +0000

    Read a remote URL and return it to clients

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 var http = require('http')
 var server = http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.end('Hello World\n')
+  var url = 'http://jsonplaceholder.typicode.com/posts/1'
+  http.get(url, function (res) {
+    var body = ''
+    res.on('data', function (chunk) {
+      body += chunk
+    })
+    res.on('end', function () {
+      var fbResponse = JSON.parse(body)
+      console.log('Got a response: ', fbResponse.body)
+      response.end(body)
+    })
+  }).on('error', function (e) {
+    console.log('Got an error: ', e)
+  })
 })
 var port = process.env.PORT || 5000
 server.listen(port, '0.0.0.0')
+
